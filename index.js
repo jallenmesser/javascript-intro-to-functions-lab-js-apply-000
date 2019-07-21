cd46b682835046857ea9edef38c503440ddd45b4
Jallen Messersmith <jallenmessersmith@gmail.com>
2019-07-21 18:01:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  string.toUpperCase === string
+  string.toLowerCase === string
   return "I can't hear you!"
+}
+
+function sayHiToGrandma(string) {
+  string.toUpperCase === string
+  return "YES INDEED!"
 }
